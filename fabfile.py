99bf03ae3e612b8d26a96d402c79555a69ae1c0b
luis-israel-velez <luis.israel.velez@gmail.com>
2018-07-29 18:24:39 -0400

    Adding install docker function

--- fabfile.py
+++ fabfile.py
@@ -12,3 +12,13 @@ env.password = 't0ps3cr3t!' #Not real password...
 @parallel
 def cmd(command):
 	sudo(command)
+
+
+
+def install_docker():
+        sudo('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
+        sudo('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"')
+        sudo('sudo apt-get update')
+        sudo('apt-cache policy docker-ce')
+        sudo('sudo apt-get install -y docker-ce')
+        sudo('sudo systemctl status docker')
